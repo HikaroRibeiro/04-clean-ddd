78efe74cc671702221173cc8a2f4ef430d9d5cd1
HikaroRibeiro <hhakawama@gmail.com>
2025-06-03 19:13:22 -0300

    ✨ Classes de Erro.
    Implementado classes de erros.

--- src/core/either.ts
+++ src/core/either.ts
@@ -2,25 +2,41 @@
 // Ou um Ou Outro, sucesso ou erro.
 
 // Left = Error
-export class Left<L> {
+export class Left<L, R> {
   readonly value: L
 
   constructor(value: L) {
     this.value = value
   }
+
+  isRight(): this is Right<L, R> {
+    return false
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
 }
 
 // Right = Sucess
-export class Right<R> {
+export class Right<L, R> {
   readonly value: R
 
   constructor(value: R) {
     this.value = value
   }
+
+  isRight(): this is Right<L, R> {
+    return true
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
 }
 
 // Trabalhando com tipagens
-export type Either<L, R> = Left<L> | Right<R>
+export type Either<L, R> = Left<L, R> | Right<L, R>
 
 export const left = <L, R>(value: L): Either<L, R> => {
   return new Left(value)
